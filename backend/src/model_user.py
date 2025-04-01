a558960a2696596285b6c2512ac1bef7b18bcc54
rka <fipu@Mac-mini-od-FIPU.local>
2025-04-01 12:57:34 +0200

    removed comments

--- backend/src/model_user.py
+++ backend/src/model_user.py
@@ -1,11 +1,6 @@
 from .database import Base
 from sqlalchemy import Column, Integer, String, Boolean, TIMESTAMP, text
 
-
-# db = sa.create_engine("sqlite:///:memory:")
-# Session = sessionmaker(bind=db)
-# Base = declarative_base()
-
 class User(Base):
     __tablename__ = "user"
 
@@ -15,5 +10,3 @@ class User(Base):
     email = Column(String, nullable=False)
     date_added = Column(TIMESTAMP(timezone=True), nullable=False, server_default=text('now()'))
     date_modified = Column(TIMESTAMP(timezone=True), nullable=True)
-
-    #what is difference between server_default and default
