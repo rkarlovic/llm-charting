c10282db7b0b613e1a13e070b18340a312e23f77
aolomoao <asmajic@unipu.hr>
2025-04-02 10:07:54 +0200

    Device optimization (DT, TB, SP)
    CSS fixes

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,8 +4,8 @@ import { BrowserRouter, Routes,Route } from 'react-router-dom'
 import './index.css'
 import Login from './pages/Login'
 import Register from './pages/Register'
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+//import { StrictMode } from 'react'
+//import { createRoot } from 'react-dom/client'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
--- frontend/src/pages/Register.tsx
+++ frontend/src/pages/Register.tsx
@@ -1,6 +1,9 @@
+import { useState } from 'react';
 import './Auth.css';
 
 const Register = () => {
+    const [termsAccepted, setTermsAccepted] = useState(false);
+
   return (
     <div className="auth-container">
         <div className="auth-box">
@@ -10,7 +13,20 @@ const Register = () => {
             <input type="text" placeholder="prezime" />
             <input type="email" placeholder="e-mail adresa" />
             <input type="password" placeholder="lozinka" />
-            <button>Registracija</button>
+
+            <label className="checkbox-container">
+                <input 
+                type="checkbox" 
+                checked={termsAccepted} 
+                onChange={() => setTermsAccepted(!termsAccepted)} 
+                />
+                <span className="custom-checkmark"></span>
+                <span className="checkbox-label">
+                Prihvaćam Jur.ai <b>Uvjete korištenja</b>
+                </span>
+            </label>
+
+            <button disabled = {!termsAccepted} className={`register-button ${termsAccepted ? 'active' : 'disabled'}`}>Registracija</button>
         </div>
     </div>
   )
